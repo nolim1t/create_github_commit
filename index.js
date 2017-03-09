ad472b2576a3a185ee25cacf7b893c7c96a12cc5
Barry <barry.teoh@perceptionz.net>
2017-03-09 20:43:41 +0800

    Restructure the repository.

---
+++ index.js
@@ -0,0 +1,7 @@
+const image_commit = require('./lib/image_commit');
+const create_commit = require('./lib/create_commit');
+
+module.exports = {
+  create_commit: create_commit,
+  image_commit: image_commit
+}
